fd1cfda14b78d7e5a0479e4f28b65afa7531664e
Brady Alleman <balleman@thtech.net>
2020-01-02 20:59:42 -0500

    switch to simplekml

--- src/build.py
+++ src/build.py
@@ -2,18 +2,15 @@
 
 import yaml
 import os
-from lxml import etree
-from pykml.factory import KML_ElementMaker as KML
-from pykml.factory import ATOM_ElementMaker as ATOM
-from pykml.factory import GX_ElementMaker as GX
-from youtube_dl.extractor import streamable
+import simplekml
 
 def main():
     target = "../target"
     if (not os.path.isdir(target)):
         os.mkdir("../target")
     
-    kml = KML.kml()
+    kml = simplekml.Kml(open=1)
+    kml.document.name = "radio-mapping"
                 
     sites = "../data/site"
     for (dirpath, dirnames, filenames) in os.walk(sites):
@@ -21,25 +18,11 @@ def main():
             filename = os.path.join(dirpath, name)
             stream = open(filename, 'r')
             site = yaml.safe_load(stream)
-            placemark = KML.Placemark(
-                KML.name(site['site']),
-                KML.description(site['name']),
-                KML.Point(
-                    KML.coordinates("{lon},{lat}".format(lon=site['loc']['lon'], lat=site['loc']['lat']))
-                    )
-                )
-            kml.append(placemark)
+            point = kml.newpoint(name=site['site'])
+            point.description = site['name']
+            point.coords = [(site['loc']['lon'], site['loc']['lat'])]
         
-    doc = KML.kml(
-        etree.Comment(' required when using gx-prefixed elements '),
-        KML.Placemark(
-            KML.name('gx:altitudeMode Example'),
-            KML.Point(
-                KML.coordinates("-77, 40")
-            )
-        ),
-    )
-    etree.ElementTree(kml).write(target + "/radio-mapping.kml", pretty_print=True)
+    kml.save(target + "/radio-mapping.kml")
 
 if __name__ == '__main__':
     main()
