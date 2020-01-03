0ee39a748fe21b0b16b7be7adf67a35bfccd24e1
Brady Alleman <balleman@thtech.net>
2020-01-02 21:15:33 -0500

    folder handling

--- src/build.py
+++ src/build.py
@@ -10,17 +10,28 @@ def main():
         os.mkdir("../target")
     
     kml = simplekml.Kml(open=1)
-    kml.document.name = "radio-mapping"
+    kml.document.name = "Radio Mapping"
+    
+    fld_phy = kml.newfolder(name="Physical Sites")
+    folders = dict()
                 
     sites = "../data/site"
     for (dirpath, dirnames, filenames) in os.walk(sites):
+        for dirname in dirnames:
+            if dirpath in folders:
+                fld = folders[dirpath].newfolder(name=dirname)
+            else:
+                fld = fld_phy.newfolder(name=dirname)
+            folders[os.path.join(dirpath, dirname)] = fld 
         for name in filenames:
             filename = os.path.join(dirpath, name)
-            stream = open(filename, 'r')
-            site = yaml.safe_load(stream)
-            point = kml.newpoint(name=site['site'])
-            point.description = site['name']
-            point.coords = [(site['loc']['lon'], site['loc']['lat'])]
+            if (filename.endswith(".yaml")):
+                stream = open(filename, 'r')
+                site = yaml.safe_load(stream)
+                fld = folders[dirpath]
+                point = fld.newpoint(name=site['site'])
+                point.description = site['name']
+                point.coords = [(site['loc']['lon'], site['loc']['lat'])]
         
     kml.save(target + "/radio-mapping.kml")
 
