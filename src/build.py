4ad593c492c6bf2628ba9dd521b0ceb54bd9a5a9
Brady Alleman <balleman@thtech.net>
2020-01-03 20:05:11 -0500

    services

--- src/build.py
+++ src/build.py
@@ -14,9 +14,10 @@ def main():
     
     fld_phy = kml.newfolder(name="Physical Sites")
     folders = dict()
+    sites = dict()
                 
-    sites = "../data/site"
-    for (dirpath, dirnames, filenames) in os.walk(sites):
+    site_path = "../data/site"
+    for (dirpath, dirnames, filenames) in os.walk(site_path):
         for dirname in dirnames:
             if dirpath in folders:
                 fld = folders[dirpath].newfolder(name=dirname)
@@ -28,6 +29,7 @@ def main():
             if (filename.endswith(".yaml")):
                 stream = open(filename, 'r')
                 site = yaml.safe_load(stream)
+                sites[site['site']] = site
                 fld = folders[dirpath]
                 point = fld.newpoint(name=site['site'])
                 point.description = site['name']
@@ -40,6 +42,40 @@ def main():
                     point.style.iconstyle.icon.href = "http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png"
                 else:
                     point.style.iconstyle.icon.href = "http://maps.google.com/mapfiles/kml/paddle/ylw-stars.png"
+    
+    fld_svc = kml.newfolder(name="Services")
+    service_path = "../data/service"
+    for (dirpath, dirnames, filenames) in os.walk(service_path):
+        for dirname in dirnames:
+            if dirpath in folders:
+                fld = folders[dirpath].newfolder(name=dirname)
+            else:
+                fld = fld_svc.newfolder(name=dirname)
+            folders[os.path.join(dirpath, dirname)] = fld 
+        for name in filenames:
+            filename = os.path.join(dirpath, name)
+            if (filename.endswith(".yaml")):
+                stream = open(filename, 'r')
+                svc = yaml.safe_load(stream)
+                fld = folders[dirpath]
+                if "type" not in svc:
+                    point = fld.newpoint(name=svc['service'])
+                    point.description = svc['service']
+                    if "callsign" in svc:
+                        site = sites[svc['site']]
+                        point.coords = [(site['loc']['lon'], site['loc']['lat'])]
+                        point.description += "<br />Callsign: " + svc['callsign']
+                        point.style.iconstyle.icon.href = "http://maps.google.com/mapfiles/kml/shapes/target.png"
+                        point.style.iconstyle.scale = 2                                      
+                elif svc['type'] == "link":
+                    line = fld.newlinestring(name=svc['service'])
+                    site0 = sites[svc['site']]
+                    site1 = sites[svc['dest']]
+                    line.coords = [(site0['loc']['lon'], site0['loc']['lat']), (site1['loc']['lon'], site1['loc']['lat'])]
+                    line.extrude = 1
+                    line.style.linestyle.width = 3
+                    line.style.linestyle.color = simplekml.Color.yellow
+                    
         
     kml.save(target + "/radio-mapping.kml")
 
